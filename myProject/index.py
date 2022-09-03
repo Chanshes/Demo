9cf1679de11e050f9f8c2b19f3a7b3da4fa640ce
xingchen <laixingchen530@163.com>
2022-09-03 22:49:54 +0800

    QQ轰炸器

---
+++ myProject/index.py
@@ -0,0 +1,10 @@
+import win32gui as gui
+import win32con as con
+import time
+# QQ轰炸器
+handle = gui.FindWindow(0, '***')
+
+for i in range(100):
+    gui.SendMessage(handle, con.WM_PASTE)
+    gui.SendMessage(handle, con.WM_KEYDOWN, con.VK_RETURN)
+    time.sleep(0.5)
