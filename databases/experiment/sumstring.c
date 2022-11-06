b8ff2b1feb682840f9b4a916afb5604be50fff10
xingchen <laixingchen530@163.com>
2022-11-07 00:43:12 +0800

    Input prompt

--- databases/experiment/sumstring.c
+++ databases/experiment/sumstring.c
@@ -6,6 +6,7 @@ int main()
     char string[MAX];
     int i,num=0,word=0;
     char c,s;
+    printf("Please enter a line of characters separated by spaces:");
     gets(string);
     for(i=0;(c=string[i])!='\0';i++)
     {
