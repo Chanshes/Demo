48ee2c28452862de0a60f5605cd1f93bd1821254
xingchen <laixingchen530@163.com>
2022-11-07 12:55:42 +0800

    将下面的代码转换为普通构造函数的写法     class Example {     constructor(name) {         this.name = name;     }     func() {         console.log(this.name)     } }

---
+++ Interview/js/classTofunction_124.js
@@ -0,0 +1,15 @@
+/*
+    将下面的代码转换为普通构造函数的写法
+    考察对类的掌握
+*/
+
+class Example { 
+    constructor(name) {
+        this.name = name;
+    }
+    func() {
+        console.log(this.name)
+    }
+}
+var e = new Example('abc');
+new e.func();
