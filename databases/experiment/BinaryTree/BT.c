43ae0f631c070382d31c1529fde0983cdbbc6dfa
xingchen <laixingchen530@163.com>
2022-11-30 00:59:37 +0800

    .BinaryTree

--- databases/experiment/BinaryTree/BT.c
+++ databases/experiment/BinaryTree/BT.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "BT.h"
 #include <windows.h>
+
+
 int main()
 {
     BTree *t;
@@ -9,6 +11,12 @@ int main()
     printf("\n");
     printf("先序遍历为：");
     PreOrder(t);
+    printf("\n");
+    printf("中序遍历为：");
+    InOrder(t);
+    printf("\n");
+    printf("后序遍历为：");
+    PostOrder(t);
     printf("\n");
     system("pause");
     return 0;
